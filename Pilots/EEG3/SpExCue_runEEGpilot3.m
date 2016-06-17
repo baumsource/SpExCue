f592245cd45a37c21c51288078c7db4bf53bab9e
baumsource <baumsource@gmail.com>
2016-06-17 09:40:43 -0400

    EEG3: updates to reduce signal distortions audible only thru TDT+ER2; main change: filter order of Butterworth bandpass reduced from 9 to 4.

--- Pilots/EEG3/SpExCue_runEEGpilot3.m
+++ Pilots/EEG3/SpExCue_runEEGpilot3.m
@@ -1,17 +1,26 @@
 % SpExCue_runEEGpilot
 
 ID = 'RB'; % RS2
+eegflag = 0; % type 1 or true to run with EEG setting
 
-% addpath(fullfile('..','..','MATLAB_general'))
-% addpath(fullfile('..','..','MATLAB_general','sofa_new','API_MO'))
-% addpath(fullfile('..','..','MATLAB_general','ltfat'))
+addpath(fullfile('..','..','MATLAB_general'))
+addpath(fullfile('..','..','..','sofa','API_MO'))
+addpath(fullfile('..','..','..','ltfat'))
 
-% ltfatstart
-% SOFAstart
+ltfatstart
+SOFAstart
+sca
 
-% behavioral pilot
-SpExCue_EEGpilot3(ID,'screenNumber',0,'azi',-90:90:90,'Nrep',12,...
-  'fnExtension','','TDToff','TbTfeedback','debugMode','skipFamiliarization')
-
-% EEG
-% SpExCue_EEGpilot(ID,'screenNumber',1,'azi',-90)
+if not(eegflag)
+    %% behavioral pilot
+    SpExCue_EEGpilot3(ID,'screenNumber',1,'azi',-90:90:90,'Nrep',1*18,...
+      'fnExtension','behav','noFeedback')
+    SpExCue_analyzeEEGpilot3_behavior([ID,'behav'])
+else
+    %% EEG monitored experiment
+    SpExCue_EEGpilot3(ID,'screenNumber',1,'azi',90,'Nrep',...
+      'fnExtension','eeg','blockedFeedback')
+    SpExCue_analyzeEEGpilot3_behavior([ID,'eeg'])
+end
+% Stimulus level calibration
+% SpExCue_EEGpilot3(ID,'screenNumber',1,'azi',0,'Nrep',12,'fnExtension','debug','debugMode','SPL',70,'dur',30)

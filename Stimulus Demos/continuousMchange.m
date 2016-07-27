7384562450dd38a27dd3c6f0289577e7f59cb543
baumsource <baumsource@gmail.com>
2016-07-27 08:12:49 -0400

    changes due to script execution

--- Stimulus Demos/continuousMchange.m
+++ Stimulus Demos/continuousMchange.m
@@ -1,13 +1,17 @@
 % listen to concatenated stimuli with regularly ordered spectral contrast
 
-ID = 'RB';
-M = [1,0];%.25,.5,.75,1,1.5];
+ID = 'S06';
+M = [1,0];
 % M = [1,0,1,0];
-pos = [0,0];
+pos = [90,0];
 flow = 800;
 fhigh = 16e3;
 
-stim = SpExCue_stim(M,ID,'pos',pos,'continuousNoise','flow',flow,'fhigh',fhigh,'SPL',60,'ARI');
+% Changes in azimuth
+% M = 1;
+% pos = [90,0;60,0];
+
+stim = SpExCue_stim(M,ID,'pos',pos,'continuousNoise','flow',flow,'fhigh',fhigh,'SPL',60,'HRCeq');
 
 % Concatenate longer set of Ms
 % concatStim = stim.sig{1};
@@ -24,18 +28,18 @@ concatStim = SpExCue_crossfade(stim.sig{1},stim.sig{2},...
 %           stim.fs,1.2,.6,.1);
         
 %% Spectrogram
-figSgram = figure;
-subplot(1,2,1)
-sgram(concatStim(:,1),stim.fs,'dynrange',60,'db')
-set(gca,'YLim',[flow-500,fhigh+500])
-title('Left')
-subplot(1,2,2)
-sgram(concatStim(:,2),stim.fs,'dynrange',60,'db')
-set(gca,'YLim',[flow-500,fhigh+500])
-title('Right')
-
-set(figSgram,'PaperUnits','centimeters','PaperPosition',[100,100,10,6])
-print(figSgram,'-r600','-dpng',mfilename)
+% figSgram = figure;
+% subplot(1,2,1)
+% sgram(concatStim(:,1),stim.fs,'dynrange',60,'db')
+% set(gca,'YLim',[flow-500,fhigh+500])
+% title('Left')
+% subplot(1,2,2)
+% sgram(concatStim(:,2),stim.fs,'dynrange',60,'db')
+% set(gca,'YLim',[flow-500,fhigh+500])
+% title('Right')
+% 
+% set(figSgram,'PaperUnits','centimeters','PaperPosition',[100,100,10,6])
+% print(figSgram,'-r600','-dpng',mfilename)
 
 %% Playback
 % pause
